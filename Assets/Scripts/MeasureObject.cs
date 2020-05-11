ffa75465042c3e7fd54aa11bd969c552232cfb28
Yaolee <yaoleeteoh@gmail.com>
2020-05-11 16:54:35 +0200

    init

---
+++ Assets/Scripts/MeasureObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MeasureObject : MonoBehaviour
+{
+    [SerializeField] private bool height;
+    [SerializeField] private bool width;
+    [SerializeField] private bool lenght;
+    
+    [SerializeField] private GameObject cube;
+
+    private TextMeshProUGUI text;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (height)
+        {
+            text.SetText((cube.transform.localScale.y * 100).ToString());
+        }
+        else if (width)
+        {
+            text.SetText((cube.transform.localScale.z * 100).ToString());
+        }
+        else if (lenght)
+        {
+            text.SetText((cube.transform.localScale.x * 100).ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/UIFollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFollowCamera : MonoBehaviour
+{
+    public Transform target;
+    private bool m_IstargetNotNull;
+
+
+    private void Start()
+    {
+        m_IstargetNotNull = target != null;
+    }
+
+    void Update()
+    {
+        if (m_IstargetNotNull)
+        {
+            transform.rotation = target.rotation;
+        }
+    }
+}
