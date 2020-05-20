ae14d42914eafd58d9777da6721f3e1af6854a6f
George <georgeantonio1998@yahoo.com>
2020-05-20 13:00:59 +0200

    Script : Measuring change color if cm allowed overreached, keep text same size

--- Assets/Scripts/MeasureObject.cs
+++ Assets/Scripts/MeasureObject.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Globalization;
 using TMPro;
 using UnityEngine;
 
@@ -8,30 +6,35 @@ public class MeasureObject : MonoBehaviour
 {
     [SerializeField] private bool height;
     [SerializeField] private bool width;
-    [SerializeField] private bool lenght;
+    [SerializeField] private bool length;
     
     [SerializeField] private GameObject cube;
 
-    private TextMeshProUGUI text;
+    [SerializeField] private Material green;
+    [SerializeField] private Material red;
+
+    private TextMeshProUGUI _text;
 
     private void Start()
     {
-        text = GetComponent<TextMeshProUGUI>();
+        _text = GetComponent<TextMeshProUGUI>();
     }
 
     private void Update()
     {
         if (height)
-        {
-            text.SetText((cube.transform.localScale.y * 100).ToString());
-        }
+            _text.SetText(((Mathf.Round(cube.transform.localScale.y * 100) * 10f) / 10f).ToString(CultureInfo.InvariantCulture) + "CM");
         else if (width)
-        {
-            text.SetText((cube.transform.localScale.z * 100).ToString());
-        }
-        else if (lenght)
-        {
-            text.SetText((cube.transform.localScale.x * 100).ToString());
-        }
+            _text.SetText(((Mathf.Round(cube.transform.localScale.z * 100) * 10f) / 10f).ToString(CultureInfo.InvariantCulture) + "CM");
+        else if (length)
+            _text.SetText(((Mathf.Round(cube.transform.localScale.x * 100) * 10f) / 10f).ToString(CultureInfo.InvariantCulture) + "CM");
+
+        _text.rectTransform.localScale = new Vector3(1,1,1);
+
+        if (cube.transform.localScale.x > 40 || cube.transform.localScale.y > 55 || cube.transform.localScale.z > 20)
+            cube.GetComponent<MeshRenderer>().material = red;
+        else
+            cube.GetComponent<MeshRenderer>().material = green;
+
     }
 }
